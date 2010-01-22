6376e80910609b3c04747559537e0a30cf55e251
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-01-22 16:26:15 +0600

    Hacked Goo.gl successfully using their own JS
    Rhino ROCKS!
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- src/main/java/com/smartitengineering/demo/googleurlshortner/App.java
+++ src/main/java/com/smartitengineering/demo/googleurlshortner/App.java
@@ -1,6 +1,7 @@
 package com.smartitengineering.demo.googleurlshortner;
 
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.zip.GZIPInputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
@@ -8,6 +9,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 /**
  * Hello world!
@@ -15,31 +18,66 @@ import org.apache.http.impl.client.DefaultHttpClient;
  */
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+      throws Exception {
+    String url =
+        "http://coding.derkeiler.com/Archive/General/comp.programming/2006-11/msg00386.html";
+    String encodedUrl = URLEncoder.encode(url, "utf-8");
+    InputStream hackScript = App.class.getClassLoader().getResourceAsStream(
+        "hackScript.js");
+    String hackScriptStr;
+    try {
+      hackScriptStr = IOUtils.toString(hackScript);
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      hackScriptStr = null;
+    }
+    System.out.println("HackString: " + hackScriptStr);
+    String authToken;
+    if (hackScript != null) {
+      Context context = Context.enter();
+      Scriptable scriptable = context.initStandardObjects();
+      String invokable =
+          hackScriptStr
+          + "\r\ngetAuthToken(\""+url+"\");";
+      Object result = context.evaluateString(scriptable, invokable, "<cmd>", 1,
+          null);
+      authToken = result.toString();
+      System.out.println("RESULT: " + authToken);
+    }
+    else {
+      return;
+    }
     HttpPost request =
         new HttpPost(
-        "http://goo.gl/api/url?user=toolbar@google.com&url=http%3A%2F%2Fkenai.com%2Fprojects%2Fsmart-cms%2Fpages%2FWorkspace&auth_token=764104184615");
+        "http://goo.gl/api/url?user=toolbar@google.com&url=" + encodedUrl
+        + "&auth_token="+authToken);
     request.addHeader("Origin",
         "chrome-extension://iblijlcdoidgdpfknkckljiocdbnlagk");
-    request.addHeader("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.43 Safari/532.5");
+    request.addHeader("User-Agent",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.43 Safari/532.5");
     request.addHeader("Accept", "*/*");
     request.addHeader("Accept-Encoding", "gzip,deflate,sdch");
     request.addHeader("Accept-Language", "en-US,en;q=0.8");
     request.addHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+
     HttpClient client = new DefaultHttpClient();
-    try {
-      HttpResponse response = client.execute(request);
-      System.out.println("Response Code: " + response.getStatusLine().
-          getStatusCode());
-      HttpEntity entity = response.getEntity();
-      InputStream input = entity.getContent();
-      String output = IOUtils.toString(new GZIPInputStream(input), "utf-8");
-      System.out.println("Encoding: " + entity.getContentEncoding());
-      System.out.println("OUTPUT CT: " + entity.getContentType().toString());
-      System.out.println("OUTPUT: " + output);
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
+    if (true) {
+      try {
+        HttpResponse response = client.execute(request);
+        System.out.println("Response Code: " + response.getStatusLine().
+            getStatusCode());
+        HttpEntity entity = response.getEntity();
+        InputStream input = entity.getContent();
+        String output = IOUtils.toString(new GZIPInputStream(input), "utf-8");
+        System.out.println("Encoding: " + entity.getContentEncoding());
+        System.out.println("OUTPUT CT: " + entity.getContentType().toString());
+        System.out.println("OUTPUT: " + output);
+      }
+      catch (Exception ex) {
+        ex.printStackTrace();
+      }
     }
   }
 }
---
+++ src/main/resources/hackScript.js
@@ -0,0 +1,72 @@
+var _0x4a2d=["\x6C\x65\x6E\x67\x74\x68","\x63\x68\x61\x72\x41\x74","\x66\x6C\x6F\x6F\x72","\x63\x68\x61\x72\x43\x6F\x64\x65\x41\x74","\x62\x79\x74\x65\x41\x72\x72\x61\x79\x5F","\x37","\x50\x4F\x53\x54","\x68\x74\x74\x70\x3A\x2F\x2F\x67\x6F\x6F\x2E\x67\x6C\x2F\x61\x70\x69\x2F\x75\x72\x6C\x3F\x75\x73\x65\x72\x3D\x74\x6F\x6F\x6C\x62\x61\x72\x40\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D\x26\x75\x72\x6C\x3D","\x75\x72\x6C","\x26\x61\x75\x74\x68\x5F\x74\x6F\x6B\x65\x6E\x3D","\x6F\x70\x65\x6E","\x6F\x6E\x6C\x6F\x61\x64","\x72\x65\x73\x70\x6F\x6E\x73\x65\x54\x65\x78\x74","\x70\x61\x72\x73\x65","\x73\x68\x6F\x72\x74\x5F\x75\x72\x6C","\x65\x72\x72\x6F\x72","\x65\x72\x72\x6F\x72\x5F\x6D\x65\x73\x73\x61\x67\x65","\x73\x75\x63\x63\x65\x73\x73","\x73\x65\x6E\x64","\x73\x68\x6F\x72\x74\x65\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x76\x61\x6C\x75\x65","\x66\x6F\x63\x75\x73","\x73\x65\x6C\x65\x63\x74","\x43\x6F\x70\x79","\x65\x78\x65\x63\x43\x6F\x6D\x6D\x61\x6E\x64","\x63\x6F\x70\x79","\x74\x79\x70\x65","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6F\x6E\x52\x65\x71\x75\x65\x73\x74","\x65\x78\x74\x65\x6E\x73\x69\x6F\x6E","\x73\x65\x74\x42\x61\x64\x67\x65\x54\x65\x78\x74","\x62\x72\x6F\x77\x73\x65\x72\x41\x63\x74\x69\x6F\x6E","\x74\x77\x69\x74\x74\x65\x72","\x67\x65\x74\x49\x74\x65\x6D","\x66\x61\x63\x65\x62\x6F\x6F\x6B","\x6D\x61\x69\x6C","\x61\x75\x74\x6F\x43\x6F\x70\x79","\x74\x72\x75\x65","\x73\x65\x74\x49\x74\x65\x6D","\x69\x6E\x63\x6C\x75\x64\x65\x54\x69\x74\x6C\x65","\x66\x61\x6C\x73\x65","\x6D\x61\x69\x6C\x74\x6F","\x64\x65\x66\x61\x75\x6C\x74","\x6F\x70\x74\x69\x6F\x6E\x73\x2E\x68\x74\x6D\x6C","\x67\x65\x74\x55\x52\x4C","\x63\x72\x65\x61\x74\x65","\x74\x61\x62\x73"];
+var getAuthToken=function (_0x5f57x2){
+  function _0x5f57x3(){
+    for(var _0x5f57x4=0,_0x5f57x5=0;_0x5f57x5<arguments[_0x4a2d[0]];_0x5f57x5++){
+      _0x5f57x4=_0x5f57x4+arguments[_0x5f57x5]&4294967295;
+    } ;
+    return _0x5f57x4;
+  } ;
+  function _0x5f57x6(_0x5f57x4){
+    _0x5f57x4=_0x5f57x4=String(_0x5f57x4>0?_0x5f57x4:_0x5f57x4+4294967296);
+    var _0x5f57x5;
+    _0x5f57x5=_0x5f57x4;
+    for(var _0x5f57x7=0,_0x5f57x8=false,_0x5f57x9=_0x5f57x5[_0x4a2d[0]]-1;_0x5f57x9>=0;--_0x5f57x9){
+      var _0x5f57xa=Number(_0x5f57x5[_0x4a2d[1]](_0x5f57x9));
+      if(_0x5f57x8){
+        _0x5f57xa*=2;
+        _0x5f57x7+=Math[_0x4a2d[2]](_0x5f57xa/10)+_0x5f57xa%10;
+      }
+      else {
+        _0x5f57x7+=_0x5f57xa;
+      } ;
+      _0x5f57x8=!_0x5f57x8;
+    } ;
+    _0x5f57x5=_0x5f57x5=_0x5f57x7%10;
+    _0x5f57x7=0;
+    if(_0x5f57x5!=0){
+      _0x5f57x7=10-_0x5f57x5;
+      if(_0x5f57x4[_0x4a2d[0]]%2==1){
+        if(_0x5f57x7%2==1){
+          _0x5f57x7+=9;
+        } ;
+        _0x5f57x7/=2;
+      } ;
+    } ;
+    _0x5f57x5=String(_0x5f57x7);
+    _0x5f57x5+=_0x5f57x4;
+    return _0x5f57x4=_0x5f57x5;
+  } ;
+  function _0x5f57xb(_0x5f57x4){
+    for(var _0x5f57x5=5381,_0x5f57x7=0;_0x5f57x7<_0x5f57x4[_0x4a2d[0]];_0x5f57x7++){
+      _0x5f57x5=_0x5f57x3(_0x5f57x5<<5,_0x5f57x5,_0x5f57x4[_0x4a2d[3]](_0x5f57x7));
+    } ;
+    return _0x5f57x5;
+  } ;
+  function _0x5f57xc(_0x5f57x4){
+    for(var _0x5f57x5=0,_0x5f57x7=0;_0x5f57x7<_0x5f57x4[_0x4a2d[0]];_0x5f57x7++){
+      _0x5f57x5=_0x5f57x3(_0x5f57x4[_0x4a2d[3]](_0x5f57x7),_0x5f57x5<<6,_0x5f57x5<<16,-_0x5f57x5);
+    } ;
+    return _0x5f57x5;
+  } ;
+  var _0x5f57xd={
+    byteArray_:_0x5f57x2,
+    charCodeAt:function (_0x5f57x4){
+      return this[_0x4a2d[4]][_0x5f57x4];
+    }
+  };
+  _0x5f57xd[_0x4a2d[0]]=_0x5f57xd[_0x4a2d[4]][_0x4a2d[0]];
+  var _0x5f57xe=_0x5f57xb(_0x5f57xd[_0x4a2d[4]]);
+  _0x5f57xe=_0x5f57xe>>2&1073741823;
+  _0x5f57xe=_0x5f57xe>>4&67108800|_0x5f57xe&63;
+  _0x5f57xe=_0x5f57xe>>4&4193280|_0x5f57xe&1023;
+  _0x5f57xe=_0x5f57xe>>4&245760|_0x5f57xe&16383;
+  var _0x5f57xf=_0x4a2d[5];
+  _0x5f57xd=_0x5f57xc(_0x5f57xd[_0x4a2d[4]]);
+  var _0x5f57x10=(_0x5f57xe>>2&15)<<4|_0x5f57xd&15;
+  _0x5f57x10|=(_0x5f57xe>>6&15)<<12|(_0x5f57xd>>8&15)<<8;
+  _0x5f57x10|=(_0x5f57xe>>10&15)<<20|(_0x5f57xd>>16&15)<<16;
+  _0x5f57x10|=(_0x5f57xe>>14&15)<<28|(_0x5f57xd>>24&15)<<24;
+  _0x5f57xf+=_0x5f57x6(_0x5f57x10);
+  return _0x5f57xf;
+  return _0x5f57xe;
+} ;
